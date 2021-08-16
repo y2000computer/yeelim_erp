b389d7d35d5ae27a2bcf4f750e9d114e4921e35b
Sam Lam <y2000computer@gmail.com>
2021-08-16 12:31:50 +0800

    update year end

--- app/erp/module/gl_year_end/controller/handle_process_inc.php
+++ app/erp/module/gl_year_end/controller/handle_process_inc.php
@@ -37,23 +37,23 @@ $report_ending_balance_grand_ttl = 0;
 
 $chart_type_name = 'Cash';
 $type_code='010';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 		
 $chart_type_name = 'Account Receivable';
 $type_code='020';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Current Assets';
 $type_code='030';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Inventory';
 $type_code='040';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Fixed Assets';
 $type_code='050';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 
 $report_brought_forward_assets_grand_ttl = $report_brought_forward_grand_ttl;
@@ -75,31 +75,31 @@ $report_ending_balance_grand_ttl = 0;
 
 $chart_type_name = 'Account Payable';
 $type_code='060';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Current Liabilities';
 $type_code='070';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Long-term Liabilities';
 $type_code='080';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Accumulated Depreciation';
 $type_code='090';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Shareholder Equity';
 $type_code='100';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Dividend';
 $type_code='110';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 $chart_type_name = 'Equity-gets closed';
 $type_code='120';  
-require __DIR__.'\handle_chart_types_inc.php';
+require __DIR__.'/handle_chart_types_inc.php';
 
 
 $report_brought_forward_liabilites_grand_ttl = $report_brought_forward_grand_ttl;
